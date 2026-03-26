8ea5300f4dfa102b094a9fedef5fa1b94d67207a
SSarygulov <s.sarygulov@mdigital.kg>
2026-03-26 17:06:33 +0600

    fixed unknown 3rd map error

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import requests
 from datetime import datetime, timedelta, timezone
 from models import Map, Match, Team
+from config import get_faceit_api_key
 
-
-map_names ={
+maps_names ={
     'de_mirage': 'Mirage',
     'de_train': 'Train',
     'de_overpass': 'Overpass',
@@ -15,7 +15,7 @@ map_names ={
     'de_nuke': 'Nuke'
 }
 
-API_KEY = your-api-key
+API_KEY = get_faceit_api_key()
 HEADERS = {'Authorization': f'Bearer {API_KEY}'}
 
 
@@ -25,63 +25,6 @@ def format_match_datetime(timestamp, timezone_abbr) -> str:
     dt = datetime.fromtimestamp(timestamp=timestamp, tz=tz)
     return f'{dt.strftime('%B')} {dt.day}, {dt.year} - {dt.strftime('%H:%M')} {{{{Abbr/{timezone_abbr}}}}}'
 
-def main(match_id):
-
-    match_url = f'https://open.faceit.com/data/v4/matches/{match_id}'
-    match_stats_url = f'https://open.faceit.com/data/v4/matches/{match_id}/stats'
-    match_json = requests.get(url=match_url, headers=HEADERS).json()
-    match_stats = requests.get(url=match_stats_url, headers=HEADERS).json()
-
-    formatted_datetime = format_match_datetime(match_json["started_at"], 'ALMT')
-
-    team1 = Team(
-        match_json["teams"]["faction1"]["faction_id"], 
-        match_json["teams"]["faction1"]["name"]
-    )
-    team2 = Team(
-        match_json["teams"]["faction2"]["faction_id"], 
-        match_json["teams"]["faction2"]["name"]
-    )
-
-    maps = []
-    for map_number in range(3):
-
-        try:
-
-            map_stats = match_stats['rounds'][map_number]
-
-            team1_stats = get_team_stats(map_stats=map_stats, faction_id=team1.id)['team_stats']
-            team2_stats = get_team_stats(map_stats=map_stats, faction_id=team2.id)['team_stats']
-
-            map = Map(
-                id=map_stats['played'],
-                name=map_names[map_stats['round_stats']['Map']],
-                team1_side='t',
-                team1_first_half=team1_stats['First Half Score'],
-                team1_second_half=team1_stats['Second Half Score'],
-                team2_first_half=team2_stats['First Half Score'],
-                team2_second_half=team2_stats['Second Half Score']
-            )
-
-            maps.append(build_map(map))
-
-        except:
-            maps.append(
-                f"    |map{map_number+1}={{{{Map|map=Unknown|finished=skip}}}}"
-                )
-
-    
-    lines = [
-        "{{Match",
-        f"    |opponent1={{{{TeamOpponent|{team1.name.lower()}}}}}|opponent2={{{{TeamOpponent|{team2.name.lower()}}}}}",
-        f"    |date={formatted_datetime}|finished=true",
-        *maps,
-        f"    |faceit={match_id}",
-        "    }}",
-    ]
-
-    return "\n".join(lines)
-
 
 def get_name(faction_id, headers):
         team_json = requests.get(
@@ -90,8 +33,8 @@ def get_name(faction_id, headers):
         
         return team_json["name"]
 
-
 def build_map(map: Map):
+
 
     if map.team1_side == "t":
         t1_t    = map.team1_first_half
@@ -120,11 +63,67 @@ def get_team_stats(map_stats: dict, faction_id: str) -> dict:
             return team
     raise ValueError(f"Team stats not found for faction_id={faction_id}")
 
+def build_match(match_id, team1_sides):
+
+    match_url = f'https://open.faceit.com/data/v4/matches/{match_id}'
+    match_stats_url = f'https://open.faceit.com/data/v4/matches/{match_id}/stats'
+    match_json = requests.get(url=match_url, headers=HEADERS).json()
+    match_stats = requests.get(url=match_stats_url, headers=HEADERS).json()
+
+    formatted_datetime = format_match_datetime(match_json["started_at"], 'ALMT')
+    
+    team1 = Team(
+        match_json["teams"]["faction1"]["faction_id"], 
+        match_json["teams"]["faction1"]["name"]
+    )
+    team2 = Team(
+        match_json["teams"]["faction2"]["faction_id"], 
+        match_json["teams"]["faction2"]["name"]
+    )
+
+    maps = []
+    voted_maps =[maps_names[voted_map] for voted_map in match_json["voting"]["map"]["pick"]]
+
+    for i, voted_map in enumerate(voted_maps):
+
+        try:
+
+            map_stats = match_stats['rounds'][i]
+
+            team1_stats = get_team_stats(map_stats=map_stats, faction_id=team1.id)['team_stats']
+            team2_stats = get_team_stats(map_stats=map_stats, faction_id=team2.id)['team_stats']
+
+            map = Map(
+                id=map_stats['played'],
+                name=voted_map,
+                team1_side=team1_sides[i],
+                team1_first_half=team1_stats['First Half Score'],
+                team1_second_half=team1_stats['Second Half Score'],
+                team2_first_half=team2_stats['First Half Score'],
+                team2_second_half=team2_stats['Second Half Score']
+            )
+
+            maps.append(build_map(map))
+        
+        except:
+            maps.append(f"    |map{i+1}={{{{Map|map={voted_map}|finished=skip}}}}")
+    
+    lines = [
+        "{{Match",
+        f"    |opponent1={{{{TeamOpponent|{team1.name.lower()}}}}}|opponent2={{{{TeamOpponent|{team2.name.lower()}}}}}",
+        f"    |date={formatted_datetime}|finished=true",
+        *maps,
+        f"    |faceit={match_id}",
+        "    }}",
+    ]
+
+    return "\n".join(lines)
+
     
 
 if __name__ == "__main__":
 
-    match_id = input('match_id=')
-    
-    print(main(match_id))
+    match_id =  input('match_id=')
+    team1_sides = input('team1_first_sides=')
+    print(build_match(match_id, team1_sides.split()))
     
